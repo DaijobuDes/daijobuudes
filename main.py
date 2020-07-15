25be554c627c7e6c49d2da40fda6d890195002d1
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-07-15 20:17:54 +0800

    Add LICENSE on top, enable file logging, add comments

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+# MIT License
+#
+# Copyright (c) 2020 Kate Aubrey Cellan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import discord
 import os
 
 from discord.ext import commands, tasks
 from itertools import cycle
+
+# LOG START
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+# LOG END
 
 # Open token file
 with open("token.txt", "r") as file:
@@ -11,21 +45,26 @@ with open("token.txt", "r") as file:
 client = commands.Bot(command_prefix = ".")
 # status = cycle(['', '', '', ''])
 
+# Change ctx.author.id to your own discord ID if you plan to self host
 async def is_owner(ctx):
     return ctx.author.id == 451974524053749780 # DaijobuDes#0870
 
+# Load plugins
 @client.command()
 @commands.check(is_owner)
 async def load(ctx, extension):
     client.load_extension(f'plugins.{extension}')
     print(f'{extension} loaded')
 
+# Unload plugins
 @client.command()
 @commands.check(is_owner)
 async def unload(ctx, extension):
     client.unload_extension(f'plugins.{extension}')
     print(f'{extension} unloaded')
 
+# Reload plugins
+# Useful for realtime plugin testing
 @client.command()
 @commands.check(is_owner)
 async def reload(ctx, extension):
