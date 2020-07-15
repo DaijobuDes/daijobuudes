29e90012dc196cc7839ca3dd052ccb8a6cfcfed9
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-07-15 20:18:02 +0800

    Update events.py

--- plugins/events.py
+++ plugins/events.py
@@ -10,7 +10,11 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Bot online.")
-
+        print("---------------------------------------------")
+        print("Bot Name: " + self.client.user.name + "#" + self.client.user.discriminator)
+        print("Bot ID: " + str(self.client.user.id))
+        print("Discord.py Version: " + discord.__version__)
+        print("---------------------------------------------")
     
 def setup(client):
     client.add_cog(Events(client))
