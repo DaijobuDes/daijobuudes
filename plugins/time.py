eb041f060d9fdbc4a0bdf1821a0300e030ebbac0
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-08-10 15:54:36 +0800

    Use functions instead of repeating lines

--- plugins/time.py
+++ plugins/time.py
@@ -5,6 +5,15 @@ from discord.ext import commands
 from ddate.base import DDate
 
 
+def emb(ctx):
+    global embed
+    embed = discord.Embed(title="Time Module", color=0x5eff00)
+    embed.set_footer(
+        text=f"Requested by {ctx.author} on {ctx.message.created_at}",
+        icon_url=ctx.author.avatar_url
+    )
+
+
 class Time(commands.Cog):
 
     def __init__(self, client):
@@ -13,11 +22,7 @@ class Time(commands.Cog):
     @commands.command()
     async def time(self, ctx):
         localtime = time.asctime(time.localtime(time.time()))
-        embed = discord.Embed(title="Time Module")
-        embed.set_footer(
-            text=f"Requested by {ctx.author} on {ctx.message.created_at}",
-            icon_url=ctx.author.avatar_url
-            )
+        emb(ctx)
         embed.add_field(
             name="Bot Local Time",
             value=f'Local time: `{localtime}` GMT+8'
@@ -26,21 +31,13 @@ class Time(commands.Cog):
 
     @commands.command()
     async def ddate(self, ctx):
-        embed = discord.Embed(title="Time Module")
-        embed.set_footer(
-            text=f"Requested by {ctx.author} on {ctx.message.created_at}",
-            icon_url=ctx.author.avatar_url
-        )
+        emb(ctx)
         embed.add_field(name="DDate", value=DDate())
         await ctx.send(embed=embed)
 
     @commands.command()
     async def at(self, ctx):
-        embed = discord.Embed(title="Time Module")
-        embed.set_footer(
-            text=f"Requested by {ctx.author} on {ctx.message.created_at}",
-            icon_url=ctx.author.avatar_url
-        )
+        emb(ctx)
         embed.add_field(name="At", value=round(time.time()))
         await ctx.send(embed=embed)
 
