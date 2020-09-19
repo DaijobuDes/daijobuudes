94e5d4da3f1a6a581ef7f1b54d4ad04a29abb1a0
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-09-20 01:48:09 +0800

    Disable suppression of errors at the moment

--- plugins/events.py
+++ plugins/events.py
@@ -63,63 +63,63 @@ class Events(commands.Cog):
             'Connected'
         )
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        consoletime = datetime.now()
-        # Command handling
-        if isinstance(error, commands.MissingRequiredArgument):
-            print(
-                f'{consoletime}'
-                u'\u001b[33;1m [WARN/LOW] \u001b[0m'
-                f'Missing required arguments was supplied.'
-            )
-            await ctx.send("Missing required arguments.")
-        if isinstance(error, commands.BadArgument):
-            print(
-                f'{consoletime}'
-                u'\u001b[33;1m [WARN/LOW] \u001b[0m'
-                f'Bad arguments were inputted.'
-            )
-            await ctx.send("Bad arguments")
-        if isinstance(error, commands.CommandNotFound):
-            print(
-                f'{consoletime}'
-                u'\u001b[37;1m [INFO/LOW] \u001b[0m'
-                f'Command does not exist.'
-            )
-        if isinstance(error, commands.TooManyArguments):
-            print(
-                f'{consoletime}'
-                u'\u001b[37;1m [INFO/LOW] \u001b[0m'
-                f'Too many arguments'
-            )
-            await ctx.send("Too many arguments")
+    # @commands.Cog.listener()
+    # async def on_command_error(self, ctx, error):
+    #     consoletime = datetime.now()
+    #     # Command handling
+    #     if isinstance(error, commands.MissingRequiredArgument):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[33;1m [WARN/LOW] \u001b[0m'
+    #             f'Missing required arguments was supplied.'
+    #         )
+    #         await ctx.send("Missing required arguments.")
+    #     if isinstance(error, commands.BadArgument):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[33;1m [WARN/LOW] \u001b[0m'
+    #             f'Bad arguments were inputted.'
+    #         )
+    #         await ctx.send("Bad arguments")
+    #     if isinstance(error, commands.CommandNotFound):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[37;1m [INFO/LOW] \u001b[0m'
+    #             f'Command does not exist.'
+    #         )
+    #     if isinstance(error, commands.TooManyArguments):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[37;1m [INFO/LOW] \u001b[0m'
+    #             f'Too many arguments'
+    #         )
+    #         await ctx.send("Too many arguments")
 
-        # Extenstion handling
-        if isinstance(error, commands.ExtensionError):
-            print(
-                f'{consoletime}'
-                u'\u001b[31;1m [ERROR/SEVERE] \u001b[0m'
-                f'Extension error'
-            )
-        if isinstance(error, commands.ExtensionNotFound):
-            print(
-                f'{consoletime}'
-                u'\u001b[37;1m [INFO/LOW] \u001b[0m'
-                f'Extension not found'
-            )
-        if isinstance(error, commands.ExtensionNotLoaded):
-            print(
-                f'{consoletime}'
-                u'\u001b[37;1m [INFO/LOW] \u001b[0m'
-                f'Extension not loaded'
-            )
-        if isinstance(error, commands.ExtensionAlreadyLoaded):
-            print(
-                f'{consoletime}'
-                u'\u001b[37;1m [INFO/LOW] \u001b[0m'
-                f'Extension already loaded'
-            )
+    #     # Extenstion handling
+    #     if isinstance(error, commands.ExtensionError):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[31;1m [ERROR/SEVERE] \u001b[0m'
+    #             f'Extension error'
+    #         )
+    #     if isinstance(error, commands.ExtensionNotFound):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[37;1m [INFO/LOW] \u001b[0m'
+    #             f'Extension not found'
+    #         )
+    #     if isinstance(error, commands.ExtensionNotLoaded):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[37;1m [INFO/LOW] \u001b[0m'
+    #             f'Extension not loaded'
+    #         )
+    #     if isinstance(error, commands.ExtensionAlreadyLoaded):
+    #         print(
+    #             f'{consoletime}'
+    #             u'\u001b[37;1m [INFO/LOW] \u001b[0m'
+    #             f'Extension already loaded'
+    #         )
 
 
 def setup(client):
