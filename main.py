2f55deaefe499fd1abf56f7bfe86c14336c46340
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2020-10-30 19:19:53 +0800

    Improve logging to console

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import discord
 import psutil
 
 from discord.ext import commands
-from plugins.color import *
+from colored_log import ColoredFormatter
 
 
 if psutil.WINDOWS:
@@ -40,20 +40,34 @@ else:
     pass
 
 
-# LOG START
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(
-    filename='discord.log', encoding='utf-8', mode='w'
-)
-handler.setFormatter(
-    logging.Formatter(
-        '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
-    )
-)
-logger.addHandler(handler)
+# LOG TO FILE START
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler(
+#     filename='discord.log', encoding='utf-8', mode='w'
+# )
+# handler.setFormatter(
+#     logging.Formatter(
+#         '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+#     )
+# )
+# logger.addHandler(handler)
 # LOG END
 
+# Console logging
+log = logging.getLogger('daijobuudes')
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+cf = ColoredFormatter("[%(asctime)s][%(name)s][%(levelname)s] = %(message)s (%(filename)s:%(lineno)d)")
+ch.setFormatter(cf)
+log.addHandler(ch)
+
+# Set log level
+log.setLevel(logging.DEBUG)
+# End of console logging
+
+# Initialize values
 start_time = time.time()
 
 # Open token file
@@ -61,7 +75,7 @@ with open("token.txt", "r") as file:
     token = file.read()
 
 # Change owner_id to your own discord ID if you plan to self host
-client = commands.Bot(command_prefix=">>", owner_id=451974524053749780)
+client = commands.Bot(command_prefix=".", owner_id=451974524053749780)
 # DaijobuDes#0870
 
 
@@ -69,14 +83,15 @@ client = commands.Bot(command_prefix=">>", owner_id=451974524053749780)
 @commands.is_owner()
 async def load(ctx, extension):
     try:
-        client.load_extension(f'plugins.{extension}')
-        print(f'{debuginfo}{extension} loaded')
+        client.load_extension(f'plugins.{extension}.')
+        log.debug(f'{extension} loaded')
+        await ctx.message.add_reaction('✅')
     except ModuleNotFoundError:
-        await ctx.send(f'No module named {extension}')
+        log.warning(f'No module named {extension}.')
+        await ctx.message.add_reaction('❌')
     except ImportError:
-        await ctx.send(f' {extension}')
-        print(f'{errorsevere}There was an error loading this {extension}'
-        )
+        log.error(f'There was an error loading this {extension}.')
+        await ctx.message.add_reaction('❌')
 
 
 # Unload plugins
@@ -85,11 +100,12 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     try:
         client.unload_extension(f'plugins.{extension}')
-        print(f'{debuginfo}{extension} unloaded'
-        )
+        log.debug(f'{extension} unloaded')
+        await ctx.message.add_reaction('✅')
     except Exception:
         await ctx.send(f'Module not loaded {extension}')
-        print(f'{warnlow}{extension} not loaded')
+        log.critical(f'{extension} not loaded yet.')
+        await ctx.message.add_reaction('❌')
 
 
 # Reload plugins
@@ -99,37 +115,30 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
     client.unload_extension(f'plugins.{extension}')
     client.load_extension(f'plugins.{extension}')
-    print(f'{debuginfo}{extension} reloaded')
+    log.debug(f'{extension} reloaded.')
+    await ctx.message.add_reaction('✅')
 
 
-@client.command(aliases=['al', 'dr'])
-@commands.is_owner()
-async def dirload(ctx, extension):
-    client.load_extension(f'{extension}')
-    print(f'{debuginfo}{extension} loaded')
+# @client.command(aliases=['al', 'dr'])
+# @commands.is_owner()
+# async def dirload(ctx, extension):
+#     client.load_extension(f'{extension}')
+#     print(f'{cotime} {debuginfo}{extension} loaded')
 
 
-@client.command(aliases=['au', 'dru'])
-@commands.is_owner()
-async def dirunload(ctx, extension):
-    client.unload_extension(f'{extension}')
-    print(
-        f'{datetime.datetime.now()}'
-        u'\u001b[36;1m [DEBUG/INFO] \u001b[0m'
-        f'{extension} unloaded'
-    )
+# @client.command(aliases=['au', 'dru'])
+# @commands.is_owner()
+# async def dirunload(ctx, extension):
+#     client.unload_extension(f'{extension}')
+#     print(f'{cotime} {debuginfo}{extension} unloaded')
 
 
-@client.command(aliases=['drr', 'drel'])
-@commands.is_owner()
-async def dirreload(ctx, extension):
-    client.unload_extension(f'{extension}')
-    client.load_extension(f'{extension}')
-    print(
-        f'{datetime.datetime.now()}'
-        u'\u001b[36;1m [DEBUG/INFO] \u001b[0m'
-        f'{filename[:-3]} loaded'
-    )
+# @client.command(aliases=['drr', 'drel'])
+# @commands.is_owner()
+# async def dirreload(ctx, extension):
+#     client.unload_extension(f'{extension}')
+#     client.load_extension(f'{extension}')
+#     print(f'{cotime} {debuginfo}{filename[:-3]} loaded')
 
 
 @client.command(aliases=['rall'])
@@ -140,21 +149,23 @@ async def reloadall(ctx):
             if filename[:-3] == 'color':
                 pass
             else:
-                client.unload_extension(f'plugins.{filename[:-3]}')
-                print(f'{debuginfo}Unloaded {filename[:-3]}')
-                client.load_extension(f'plugins.{filename[:-3]}')
-                print(f'{debuginfo}Loaded {filename[:-3]}')
-    print(f'{debuginfo}All plugins reloaded.')
-    await ctx.send('All plugins reloaded.')
+                try:
+                    client.unload_extension(f'plugins.{filename[:-3]}')
+                    log.debug(f'Unloaded {filename[:-3]}')
+                    client.load_extension(f'plugins.{filename[:-3]}')
+                    log.debug(f'Loaded {filename[:-3]}')
+                except Exception:
+                    log.critical(f'An exception occured in {filename[:-3]}.')
+                    await ctx.message.add_reaction('❌')
+                    break
+    log.debug('All plugins reloaded.')
+    await ctx.message.add_reaction('✅')
 
 
 for filename in os.listdir('./plugins'):
     if filename.endswith('.py'):
-        if filename[:-3] == 'color':
-            pass
-        else:
-            client.load_extension(f'plugins.{filename[:-3]}')
-            print(f'{debuginfo}{filename[:-3]} loaded')
+        client.load_extension(f'plugins.{filename[:-3]}')
+        log.debug(f'{filename[:-3]} loaded')
 
 
 @client.command()
@@ -162,9 +173,14 @@ async def uptime(ctx):
     current_time = time.time()
     difference = int(round(current_time-start_time))
     text_time = str(datetime.timedelta(seconds=difference))
-    print(f'{infolow}Uptime: {text_time}')
+    log.info(f'Uptime: {text_time}')
     embed = discord.Embed(title=None, color=0x519ba4)
     embed.add_field(name="Uptime", value=text_time)
     await ctx.send(embed=embed)
+
+
+@client.command(pass_context=True)
+async def servers(ctx):
+    await ctx.send("I'm in " + str(len(client.guilds)) + " servers")
 
 client.run(token)
